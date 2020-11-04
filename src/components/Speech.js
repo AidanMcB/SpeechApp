d23a7bc5f6ec1e7ef56c7e2a31cfbadf8b0a7fbf
Aidan McBride <aidankmcbride@gmail.com>
2020-11-04 17:32:41 -0500

    first comit

---
+++ src/components/Speech.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+
+export default function Speech (){
+
+    return(
+        <div>
+            <h1>Hi There</h1>
+        </div>
+    )
+}
